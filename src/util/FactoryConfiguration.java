8b566eed95629f9385f355ee9492aa3eb24201c7
IsuruVithanage <isuruvithanagemv@gmail.com>
2021-12-28 02:42:14 +0530

    Second Page

--- src/util/FactoryConfiguration.java
+++ src/util/FactoryConfiguration.java
@@ -5,12 +5,22 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private final SessionFactory sessionFactory;
 
     private FactoryConfiguration() {
         Configuration configuration = new Configuration();
+        Properties p=new Properties();
+        try {
+            p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        configuration.setProperties(p);
         configuration.addAnnotatedClass(Student.class);
         sessionFactory = configuration.buildSessionFactory();
     }
